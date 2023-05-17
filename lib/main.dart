e8b706c5edb2cb8db864ac2091d54874e582eb6f
elevencode <archcores@gmail.com>
2023-05-17 12:21:13 +0500

    initial

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RackApp());
+}
+
+class RackApp extends StatelessWidget {
+  const RackApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
